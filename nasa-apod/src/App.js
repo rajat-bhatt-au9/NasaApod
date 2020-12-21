3e20f10847755e5cb087fcbb7fc35e104808ba84
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2020-12-21 23:38:39 +0530

    daily apod completed

---
+++ nasa-apod/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './components/Home';
+import NasaPhoto from './components/NasaPhoto';
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="app">
+            <Route path='/' exact component={Home} />
+            <Route path='/nasaphoto' component={NasaPhoto} />
+            
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
